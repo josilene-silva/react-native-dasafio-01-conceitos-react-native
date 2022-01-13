85ccbdb1e5734a193acab2b1d243f1beb4c95b7b
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-01-13 01:20:23 -0400

    feat: add new task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,12 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const task: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+    setTasks((oldTasks) => [...oldTasks, task]);
   }
 
   function handleToggleTaskDone(id: number) {
